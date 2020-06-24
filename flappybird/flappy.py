7048dfdffc53ae976bdb66a57b2ae711cee2291e
Gamer MI <prabhu.desai@gmail.com>
2020-06-24 11:37:44 -0400

    start of NEAT integration
    Co-Authored-By: prabhuvd <prabhuvd@users.noreply.github.com>

--- flappybird/flappy.py
+++ flappybird/flappy.py
@@ -457,3 +457,17 @@ def main():
 
 
 main()
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome,neat.DefaultReproduction,
+                            neat.DefaultSpeciesSet,neat.DefaultStagnation,config_path)
+
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    winner = p.run(,50)
+
+if __name__  == "__main__":
+    local_dir= os.path.dirname(__file__)
+    config_path = os.path.join(local_dir,"config-feedforward.txt")
+    run(config_path)
